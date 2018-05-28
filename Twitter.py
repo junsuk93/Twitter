0e8d7985f2eb390923314c43786ae1efa80bdd3f
강준모 <ktj4839@gmail.com>
2018-05-28 19:12:57 +0900

    HTML 서버 화면 수정

--- Twitter.py
+++ Twitter.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/main', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        # first = request.form['First']
+        # second = request.form['Second']
+        result = request.form
+        return render_template('main.html', result=result)
 
 
 if __name__ == '__main__':
